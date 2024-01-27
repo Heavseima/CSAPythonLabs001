d36b751a86e2fbd5fa9034f320d2562033f0e9f0
Heavseima <2023440heav@aupp.edu.kh>
2024-01-27 21:34:10 +0700

    ok

--- process-string.py
+++ process-string.py
@@ -5,7 +5,27 @@
 # * "String"      -> "SSttrriinngg"
 # * "Hello World" -> "HHeelllloo  WWoorrlldd"
 # * "1234!_ "     -> "11223344!!__  "
+# create a function to double each character of a string
+def letter(user_input):
+    
+    # create an empty string to store the new string after convert
+    repeat = ""
+    
+    # use for to loop every character of the string
+    for character in user_input:
+        
+        # every character of the string will be double and add to the new empty string
+        repeat += character * 2
+        
+    # return the new string
+    return repeat
+ 
+# ask for user input about what they wanna convert
+user = input("Please enter the word: ")
 
+# call the function and insert the input into the parameter of the function
+print(letter(user_input=user))
+    
 
 # Application 2
 #  Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
@@ -19,6 +39,19 @@
 
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-user_range = input("Enter a range of letters (e.g., a-z): ")
 
+user2 = input("Enter a range of letters (e.g., a-z): ")
 
+def expand_range(user_range):
+    # Split the input range at the hyphen
+    start, end = user_range.split('-')
+    
+    # Check if the start character is lowercase
+    if start.islower():
+        # Create a string by joining characters using list comprehension and ASCII values
+        return ''.join(chr(i) for i in range(ord(start), ord(end) + 1))
+    else:
+        # Create an uppercase string by joining characters using list comprehension and ASCII values
+        return ''.join(chr(i).upper() for i in range(ord(start.lower()), ord(end.lower()) + 1))
+    
+print(expand_range(user2))
